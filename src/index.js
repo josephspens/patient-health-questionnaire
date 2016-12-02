a91ba30529c8193b77d5c58f6ca8b95fe2008bec
Joseph Spens <joseph@spens.us>
2016-12-01 23:58:34 -0500

    Add a redux store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 
 import App from './components/app';
-// import store from './store';
+import store from './store';
 import 'normalize-css';
 
 // Needed for onTouchTap
