2a44d97520f1c47bed67b8dfade41d3c73f98aaa
Joseph Spens <joseph@spens.us>
2016-12-02 00:20:19 -0500

    Add a material ui stepper

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import Screener from '../screener';
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <Screener />
       </div>
     );
   }
