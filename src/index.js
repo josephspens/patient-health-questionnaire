7cb9025f045358cfd45a2e607583e33a722aa525
Joseph Spens <joseph@spens.us>
2016-12-01 23:40:53 -0500

    Set up initial react scaffolding

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+
+import App from './components/app';
+// import store from './store';
+import 'normalize-css';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <MuiThemeProvider>
+      <Router history={syncHistoryWithStore(browserHistory, store)}>
+        <Route path="/" component={App} />
+      </Router>
+    </MuiThemeProvider>
+  </Provider>,
   document.getElementById('root')
 );
