a671beb64b48f313e87e7ca31c627d8370043320
Joseph Spens <joseph@spens.us>
2016-12-07 00:18:37 -0500

    Add node server

---
+++ server/app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.resolve(__dirname, '..', 'build', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+app.get('*', function(req, res, next) {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('index', { title: 'Error' });
+});
+
+module.exports = app;
